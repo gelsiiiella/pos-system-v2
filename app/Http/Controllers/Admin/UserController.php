c422f939c4b4059d89d6582e894dc59e807d5da5
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-05-19 23:26:19 +0800

    chore: Update UserController to fetch and pass 'employee' role users to the view

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::role('employee')->get();
+
+        return view('admin.users.index', [
+            'users' => $users,
+        ]);
     }
 
     /**
