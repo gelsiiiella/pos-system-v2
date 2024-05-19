05af6c03742c18e302d0b1ea317aa557bba62875
gelsiiiella <140424378+gelsiiiella@users.noreply.github.com>
2024-05-19 23:16:27 +0800

    added code

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware(['auth', 'verified', 'role:admin'])->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
 });
