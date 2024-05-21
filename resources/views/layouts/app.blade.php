293115b5702a9edb69777006364cb03394ba5d21
gelsiiiella <140424378+gelsiiiella@users.noreply.github.com>
2024-05-21 10:53:56 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta content="width=device-width, initial-scale=1" name="viewport">
         <meta content="{{ csrf_token() }}" name="csrf-token">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'JULS POS SYSTEM') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net" rel="preconnect">
